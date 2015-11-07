cea873045e7487404e92fd4d2b55ee6a7dd912a9
汪相位 <815362031@qq.com>
2015-11-07 23:01:47 +0800

    初始化项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by PhaseWong on 15/11/7.
+//  Copyright © 2015年 PhaseWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by PhaseWong on 15/11/7.
+//  Copyright © 2015年 PhaseWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
